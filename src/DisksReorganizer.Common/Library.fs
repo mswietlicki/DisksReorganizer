65dd6cdf3d994aa2109a7011ebe62af3ad15e072
Mateusz Świetlicki <mateusz@swietlicki.net>
2018-01-21 00:33:27 +0100

    Reorganize projects

---
+++ src/DisksReorganizer.Common/Library.fs
@@ -0,0 +1,5 @@
+namespace DisksReorganizer.Common
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ src/DisksReorganizer.Scanner.CLI/Program.fs
@@ -0,0 +1,48 @@
+﻿open System
+open System.IO
+open System.Security.Cryptography
+open LiteDB
+open LiteDB.FSharp
+
+type File = {
+    Id: Guid
+    Name: string
+    Extension: string
+    Hash: string
+    Size: int64
+    SourceName: string
+    Source: string
+    Path: string
+}
+let clearHash (s:string) = s.Replace("-", "").ToLower()
+let computeHash (f:FileInfo) = File.ReadAllBytes(f.FullName) |> SHA1.Create().ComputeHash |> BitConverter.ToString |> clearHash
+let fileInfoToFile (f:FileInfo, sourceName:string, source:string) : File = {
+    Id = Guid.NewGuid();
+    Name = f.Name;
+    Extension = f.Extension;
+    Hash = computeHash f;
+    Size = f.Length;
+    Source = source;
+    SourceName = sourceName;
+    Path = f.FullName;
+}
+
+[<EntryPoint>]
+let main argv =
+    if (argv.Length < 2) then
+        printfn "Missing arguments! [SourceName] [SourcePath]"
+        exit 0
+    let sourceName = argv.[0]
+    let scanDir = argv.[1]
+
+    let findFiles = DirectoryInfo(scanDir).EnumerateFiles "*"
+
+    let mapper = FSharpBsonMapper()
+    use db = new LiteDatabase("..\\..\\DisksReorganizer.db", mapper)
+    let filesColl = db.GetCollection<File>()
+
+    findFiles
+        |> Seq.map (fun f -> fileInfoToFile (f, sourceName, scanDir))
+        |> Seq.map (fun f -> (filesColl.Insert(f),f))
+        |> Seq.iter (fun (i,f) -> printfn "File insert: %s %s" f.Hash f.Path)
+    0 // return an integer exit code
